9dc56233eaef835da3b6650da13638b286e66811
wolfyeva <81672797+wolfyeva@users.noreply.github.com>
2023-03-03 15:20:46 +0800

    Update court_reference.py
    Tennis to Badminton

--- court_reference.py
+++ court_reference.py
@@ -7,19 +7,22 @@ class CourtReference:
     """
     Court reference model
     """
+
     def __init__(self):
-        self.baseline_top = ((286, 561), (1379, 561))
-        self.baseline_bottom = ((286, 2935), (1379, 2935))
-        self.net = ((286, 1748), (1379, 1748))
-        self.left_court_line = ((286, 561), (286, 2935))
-        self.right_court_line = ((1379, 561), (1379, 2935))
-        self.left_inner_line = ((423, 561), (423, 2935))
-        self.right_inner_line = ((1242, 561), (1242, 2935))
-        self.middle_line = ((832, 1110), (832, 2386))
-        self.top_inner_line = ((423, 1110), (1242, 1110))
-        self.bottom_inner_line = ((423, 2386), (1242, 2386))
-        self.top_extra_part = (832.5, 580)
-        self.bottom_extra_part = (832.5, 2910)
+        x=0
+        y=0
+        self.baseline_top = ((0, 0), (6100, 0))
+        self.baseline_bottom = ((0, 13400), (6100, 13400))
+        self.net = ((0, 6700), (6100, 6700))
+        self.left_court_line = ((0, 0), (0, 13400))
+        self.right_court_line = ((6100, 0), (6100, 13400))
+        self.left_inner_line = ((460, 0), (460, 13400))
+        self.right_inner_line = ((6100-500, 0), (6100-500, 13400))
+        self.middle_line = ((500+2530, 0), (500+2530, 0))
+        self.top_inner_line = ((0, 760), (6100, 760))
+        self.bottom_inner_line = ((0, 13400-760), (6100, 13400-760))
+        self.top_extra_part = (500+2530, 0)
+        self.bottom_extra_part = (500+2530, 13400)
 
         self.court_conf = {1: [*self.baseline_top, *self.baseline_bottom],
                            2: [self.left_inner_line[0], self.right_inner_line[0], self.left_inner_line[1],
@@ -40,15 +43,15 @@ class CourtReference:
                            11: [self.middle_line[0], self.top_inner_line[1], self.middle_line[1],
                                 self.bottom_inner_line[1]],
                            12: [*self.bottom_inner_line, self.left_inner_line[1], self.right_inner_line[1]]}
-        self.line_width = 1
-        self.court_width = 1117
-        self.court_height = 2408
-        self.top_bottom_border = 549
-        self.right_left_border = 274
+        self.line_width = 40
+        self.court_width = 6100
+        self.court_height = 13400
+        self.top_bottom_border = x
+        self.right_left_border = y
         self.court_total_width = self.court_width + self.right_left_border * 2
         self.court_total_height = self.court_height + self.top_bottom_border * 2
 
-        self.court = cv2.cvtColor(cv2.imread('court_configurations/court_reference.png'), cv2.COLOR_BGR2GRAY)
+        #self.court = cv2.cvtColor(cv2.imread('court_configurations/court_reference.png'), cv2.COLOR_BGR2GRAY)
 
     def build_court_reference(self):
         """
@@ -113,3 +116,4 @@ class CourtReference:
 if __name__ == '__main__':
     c = CourtReference()
     c.build_court_reference()
+    c.save_all_court_configurations()
